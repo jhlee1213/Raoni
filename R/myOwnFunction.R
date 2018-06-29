49a010c90498557f16dec16fca83f73432d86092
JaehoonLee <jhlee1213@gmail.com>
2018-06-29 11:06:30 +0900

    Update myOwnFunction.R
    update Round

--- R/myOwnFunction.R
+++ R/myOwnFunction.R
@@ -1,7 +1,15 @@
 
 Floor<-function(v,digits=0){tmp<-v*10^digits; v1<-floor(tmp)/10^digits; return(v1)}
-Round<-function(v,digits=0){tmp<-v*10^digits; idx<-which(tmp-floor(tmp)>=0.5); v1<-floor(tmp)/10^digits; if(length(idx)>0){v1[idx]<-(floor(tmp[idx])+1)/10^digits} ;  return(v1)}
-mape<-function(v1,v2){abs((v2-v1)/v1)}
+
+Round<-function(v,digits=0){
+  posneg = sign(x)
+  z = abs(x)*10^digits
+  z = z + 0.5
+  z = trunc(z)
+  z = z/10^digits
+  z*posneg
+}
+
 get_density<- function(x, y, n = 100) {
     dens <- MASS::kde2d(x = x, y = y, n = n)
     ix <- findInterval(x, dens$x)
@@ -11,4 +19,5 @@ get_density<- function(x, y, n = 100) {
 }
 
 mean0<-function(v)mean(na.omit(v))
+
 sum0<-function(v)sum(na.omit(v))
